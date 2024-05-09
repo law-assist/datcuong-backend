c39b22a5440936b60119cf0d002df96d50f5079c
KuangHcmUT <quang.nguyenqnvnbk@hcmut.edu.vn>
2024-05-10 05:22:37 +0700

    fixxing crawler

--- src/services/Crawler/regex.ts
+++ src/services/Crawler/regex.ts
@@ -1,8 +1,13 @@
 
+
+
 // const chuongRegex = /^Chương (I{1,3}|IV|V|VI{0,3}|IX|X)\b/;
+const phanRegex = /^Phần thứ (\d|(mười một)|(mười hai)|(mười ba)|(mười bốn)|(mười lăm)|(mười sáu)|(mười bảy)|(mười tám)|(mười chín)|(hai mươi)|nhất|hai|ba|bốn|năm|sáu|bảy|tám|chín|mười)/i;
+
 const chuongRegex = /^Chương (?:[1-9]\d*|X{0,3}(IX|IV|V?I{0,3}))\b/;
 
 const mucRegex = /^Mục (\d+)(\.|:)/;
+const tieuMucRegex = /^Tiểu mục (\d+)(\.|:)/;
 const dieuRegex = /^Điều (\d+)\./;
 // const khoanRegex = /^(\d+)\./;
 
@@ -14,7 +19,7 @@ const diemRegex2 = /^([\p{L}])\)/u;
 const khoanRegex = /^(\d+)\./;
 
 const isOpen = /^[“"]/;
-const isClose = /["”]\.$/;
+const isClose = /\.("|”)\.?$/;
 
 
-export { chuongRegex, mucRegex, dieuRegex, khoanRegex, diemRegex1, diemRegex2, isOpen, isClose};
+export { phanRegex, chuongRegex, mucRegex, tieuMucRegex, dieuRegex, khoanRegex, diemRegex1, diemRegex2, isOpen, isClose};
